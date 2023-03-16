ae6670aa0e63659e1c7dc3f0ca2fd55c1cdbcdc1
emy2023 <imanesadoqi@gmail.com>
2023-03-16 15:51:48 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - this the main function
+ *
+ *returns - 0
+ */
+
+int main(void)
+
+{
+    puts("Programming is like building a multilingual puzzle");
+
+    return (0);
+
+}
