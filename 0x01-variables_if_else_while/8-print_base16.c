645e3005e945cbb8b351c3f3394bc3886f7b9f7e
emy2023 <imanesadoqi@gmail.com>
2023-03-20 02:10:10 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 15 ; a++)
+	{
+		putchar();
+	}
+	putchar('\n');
+	return (0);
+}
+
