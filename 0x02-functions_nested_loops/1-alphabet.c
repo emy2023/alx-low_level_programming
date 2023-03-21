622a6916fe9ab5892931cf92835b59a7bceb5abe
emy2023 <imanesadoqi@gmail.com>
2023-03-21 13:23:25 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  print_alphabet - function that print all alphabet in lower case
+ *  Return: Always 0(succes)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+
+	}
+	_putchar('\n');
+}
