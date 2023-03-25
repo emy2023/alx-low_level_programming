95f505ddc8a8819956a7cce2f6691e18533eb84e
emy2023 <imanesadoqi@gmail.com>
2023-03-24 17:38:57 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_numbers - prints numbers form 0 to 9
+ * Return: Always 0(succes)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57;)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
