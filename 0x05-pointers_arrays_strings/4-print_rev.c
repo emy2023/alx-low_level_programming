dd86b6676c7c372114f109fc3316dc4fb3ad2c39
emy2023 <imanesadoqi@gmail.com>
2023-03-27 18:21:40 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print the reverse of string
+ * @s: the input character
+ * return: nothing
+ */
+void print_rev(char *s)
+{
+	int lent = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		lent++;
+	}
+	while (lent >= 0)
+	{
+		_putchar(*s);
+		s--;
+		lent--;
+	}
+	_putchar('\n');
+}
