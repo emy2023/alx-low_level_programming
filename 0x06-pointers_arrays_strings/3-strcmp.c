5fdf7121a2b65ef396b0eb589487cd2add16d24c
emy2023 <imanesadoqi@gmail.com>
2023-03-30 10:26:03 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (s1[i] != '\0')
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i == j)
+	if (s1[i] == s2[i])
 	{
 		return (0);
 	}
 	else
 	{
-		return (i - j);
+		return (s1[i] - s2[i]);
 	}
 }
