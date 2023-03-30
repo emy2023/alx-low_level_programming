5d781d8bb4c15acdb635716dbd5c1674633fa7a1
emy2023 <imanesadoqi@gmail.com>
2023-03-30 15:42:45 -0700

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words
+ * @a: input characters
+ * Return: the new value of characters
+ */
+char *cap_string(char *a)
+{
+	int i = 0;
+	int cap_next = 1;
+
+	while (a[i] != '\0')
+	{
+		if ((cap_next == 1 && a[i] >= 'a') && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		cap_next = 0;
+		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+		{
+			cap_next = 1;
+		}
+		i++;
+	}
+	return (a);
+}
