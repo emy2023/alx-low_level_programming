38e10830009f72bff1f581d9605e98643fb2b485
emy2023 <imanesadoqi@gmail.com>
2023-03-30 18:22:20 -0700

    lee

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,20 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_nums = "4433007711";
 
 	for (i = 0 ; str[i]; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; leet_chars[j]; j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+			str[i] = leet_nums[j];
+			break;
+			}		
+		}
 	}
 	return (str);
 }
