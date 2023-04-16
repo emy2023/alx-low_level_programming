3e73acb85d51bc433567da79700922da2d2f1459
emy2023 <imanesadoqi@gmail.com>
2023-04-16 06:44:07 -0700

    atoi

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,26 @@
+#include "main.h"
+/**
+ * atoi - convert string to integer
+ * @s: input string
+ * Return: intger value
+ */
+int atoi(char *s)
+{
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
+	while (!(s[i] <= '9' && s[i] >= 0) && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	while ((s[i] <= '9' && s[i] >= 0) && s[i] != '\0')
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
+}
