f427040b8bd6303e0dfe49c0e3ac9ed8488cadd0
emy2023 <imanesadoqi@gmail.com>
2023-04-17 03:17:27 -0700

    newstr

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - return a pointer newly allocated space in memory
+ * @str: input string
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	int lenght = 0;
+	int i;
+
+	if (str == 0)
+	{
+		return (0);
+	}
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	newstr = (char*)malloc((lenght + 1) * sizeof(char));
+	if (newstr == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < lenght; i++)
+	{
+		newstr[i] = str[i];
+	}
+	return (newstr);
+}
