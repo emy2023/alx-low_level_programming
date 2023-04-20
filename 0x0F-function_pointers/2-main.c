b430d8dbc1ffa3f353e296442e6ca371b5145ee0
emy2023 <imanesadoqi@gmail.com>
2023-04-20 16:58:46 -0700

    index

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -9,7 +9,7 @@
 */
 int is_98(int elem)
 {
-return (98 == elem);
+return (elem == 98);
 }
 
 /**
@@ -42,7 +42,10 @@ return (elem == 98 || -elem == 98);
 */
 int main(void)
 {
-int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+int array[20] = {
+	0, -98, 98, 402, 1024, 4096, -1024, -98,
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98
+};
 int index;
 
 index = int_index(array, 20, is_98);
